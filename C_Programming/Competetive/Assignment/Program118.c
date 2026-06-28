9a16db8ce3520ddb790b7dff59fda5823293bca3
Vedant Nandanwar <vedantnandanwar22@gmail.com>
2026-06-29 00:42:34 +0530

    Added Program116 to Program120

---
+++ C_Programming/Competetive/Assignment/Program118.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char cCount = '\0';
+    
+
+    for(i = 1; i <= iRow; i++)
+    {
+        if(i % 2 == 0)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                printf("%d\t",j);
+            }
+        }
+        else
+        {
+            for(j = 1, cCount = 'a'; j <= iCol; j++, cCount++)
+            {
+                printf("%c\t",cCount);
+            }
+        }
+        printf("\n");  
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns :");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
