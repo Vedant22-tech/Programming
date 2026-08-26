3dba0248b0a5bbd842204efe08c579981b70ed16
Vedant Nandanwar <vedantnandanwar22@gmail.com>
2026-08-27 04:48:01 +0530

    Add Java programming logic building practice

---
+++ Java_Programming/Practice/LB/program68.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class program68
+{
+   public static void main(String A[])
+   {
+      Scanner sobj = new Scanner(System.in);
+
+      int iValue = 0;
+      boolean bRet = false;
+       
+      System.out.println("Enter number : ");
+      iValue = sobj.nextInt();
+
+      NumberX nobj = new NumberX();
+
+      bRet = nobj.CheckPrime(iValue);
+
+      if(bRet  == true)
+      {
+         System.out.println("It is prime");
+      }
+      else
+      {
+         System.out.println("It is not prime");
+      }
+   }
+}
+
+class NumberX
+{
+   public boolean CheckPrime(int iNo)
+   {
+      // Logic
+      int iCnt = 0;
+      boolean bFlag = true;
+
+      for(iCnt = 2; iCnt <= (iNo / 2); iCnt++)
+      {
+         if((iNo % iCnt) == 0)
+         {
+            bFlag = false;
+            break;
+         }
+      }
+
+      return bFlag;
+
+   }
+   
+}
+
+// Time Complexity : O(N/2)
+// Where N >= 0
