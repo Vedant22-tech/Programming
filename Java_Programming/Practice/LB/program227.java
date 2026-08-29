726f24a288f6ff499f00b22cb42a8bfbf5369a32
Vedant Nandanwar <vedantnandanwar22@gmail.com>
2026-08-29 14:34:03 +0530

    Add Java programs

---
+++ Java_Programming/Practice/LB/program227.java
@@ -0,0 +1,59 @@
+/*
+    iRow = 4
+    iCol = 4
+
+    a
+    b   c 
+    d   e   f
+    g   h   i   j
+
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+        char ch = '\0';
+
+        // Filter for Diagonal Parameter
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid Parameter");
+            System.out.println("Number of rows and columns should be same");
+            return;
+        }
+
+        for(i = 1, ch = 'a'; i <= iRow; i++)
+        {
+            for(j = 1; j <= i; j++)
+            {   
+                System.out.print(ch+"\t");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program227
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter Number of Rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Columns");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+    }
+}
